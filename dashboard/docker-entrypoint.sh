88cd083bcd4ee8ee90487ccca374394ec9e0fea8
jasongong <JasonGong_gzj@outlook.com>
2026-05-22 15:08:51 +0800

    pass transcripts dir in docker

--- dashboard/docker-entrypoint.sh
+++ dashboard/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -eu
 
+ARGS=""
+if [ -n "${DASHBOARD_TRANSCRIPTS_DIR:-}" ] && [ -d "${DASHBOARD_TRANSCRIPTS_DIR}" ]; then
+  ARGS="$ARGS --transcripts-dir ${DASHBOARD_TRANSCRIPTS_DIR}"
+fi
+
 exec python -m dashboard.server.main \
   --project "${DASHBOARD_PROJECT}" \
   --host "${DASHBOARD_HOST}" \
   --port "${DASHBOARD_PORT}" \
+  $ARGS \
   "$@"
